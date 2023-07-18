e1b6c8df807badb86433b7ce1029190dc165604a
Tong Lin <wowawawoa@gmail.com>
2023-07-18 15:25:57 +0930

    feat: add event components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-function HomePage() {
+import { getFeaturedEvents } from "@/dummy-data";
+import EventList from "../components/events/event-list";
+
+function HomePage () {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
-  );
+  )
 }
 
 export default HomePage;
