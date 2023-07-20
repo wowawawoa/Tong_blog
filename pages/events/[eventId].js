f6bf058e5f32079df2b4c149121b16f6902e876d
Tong Lin <wowawawoa@gmail.com>
2023-07-20 11:53:26 +0930

    feat: add general layout wrapper component

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,22 +1,34 @@
 import { useRouter } from "next/router";
 import { getEventById } from "../../dummy-data";
 import { Fragment } from "react";
+import EventSummary from "../../components/event-deatil/event-summary";
+import EventLogistics from "../../components/event-deatil/event-logistics";
+import EventContent from "../../components/event-deatil/event-content";
 
-function EventDetailPage () {
+function EventDetailPage() {
   const router = useRouter();
 
   const eventId = router.query.eventId;
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No event found!</p>
+    return <p>No event found!</p>;
   }
 
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
-  )
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
 export default EventDetailPage;
