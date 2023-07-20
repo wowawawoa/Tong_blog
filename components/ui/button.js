6e7be45412ccb1051b72fdd9afea4be9e94a9e7a
Tong Lin <wowawawoa@gmail.com>
2023-07-20 12:36:03 +0930

    feat: add event search component

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,11 +1,20 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import classes from './button.module.css';
+import classes from "./button.module.css";
 
-function Button (props) {
+function Button(props) {
+  if (props.link) {
+    return (
+      <Link href={props.link} className={classes.btn}>
+        {props.children}
+      </Link>
+    );
+  }
   return (
-    <Link href={props.link} className={classes.btn}>{props.children}</Link>
-  )
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
