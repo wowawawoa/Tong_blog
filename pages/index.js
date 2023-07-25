d846ebfe15b55a54a433f2a71c75ebd60fec7275
Tong Lin <wowawawoa@gmail.com>
2023-07-25 12:23:02 +0930

    feat: getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
-import { getFeaturedEvents } from "@/dummy-data";
-import EventList from "../components/events/event-list";
+import fs from "fs/promises";
+import path from "path";
 
-function HomePage () {
-  const featuredEvents = getFeaturedEvents();
-
+function HomePage(props) {
+  const { products } = props;
   return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
-  )
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    // props: {
+    //   products: [
+    //     { id: "p1", title: "Product 1" },
+    //     { id: "p2", title: "Product 2" },
+    //     { id: "p3", title: "Product 3" },
+    //   ],
+    // },
+    props: {
+      products: data.products
+    },
+  };
 }
 
 export default HomePage;
