719c58491ca33a9dff156bd0b4ef8e0b0b87ee6f
Tong Lin <wowawawoa@gmail.com>
2023-07-26 14:12:26 +0930

    feat: working on fallback pages

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,37 @@
 import fs from "fs/promises";
 import path from "path";
+import Link from "next/link";
 
 function HomePage(props) {
   const { products } = props;
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
 }
 
-export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+export async function getStaticProps(context) {
+  console.log("(Re-)Generating...");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     // props: {
@@ -26,8 +42,9 @@ export async function getStaticProps() {
     //   ],
     // },
     props: {
-      products: data.products
+      products: data.products,
     },
+    revalidate: 10,
   };
 }
 
