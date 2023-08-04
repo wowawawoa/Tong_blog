01156a1fbd45e4ffd83d259525b575fb4f7417b3
Tong Lin <wowawawoa@gmail.com>
2023-08-04 16:33:54 +0930

    feat: add dummy posts

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,23 +1,30 @@
-import classes from './post-item.module.css';
-import Link from 'next/link';
-import Image from 'next/image';
+import classes from "./post-item.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 function PostItem(props) {
   const { title, image, excerpt, date, slug } = props.post;
 
-  const formattedDate = new Date(date).toLocaleDateString('en-AU', {
-    day: 'numeric',
-    month: 'numeric',
-    year: 'numeric'
+  const formattedDate = new Date(date).toLocaleDateString("en-AU", {
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
   });
 
   const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
 
   return (
     <li className={classes.post}>
-      <Link>
+      <Link href={linkPath}>
         <div className={classes.image}>
-          <Image src={imagePath} alt={title} width={300} height={200} />
+          <Image
+            src={imagePath}
+            alt={title}
+            width={300}
+            height={200}
+            layout="responsive"
+          />
         </div>
         <div className={classes.content}>
           <h3>{title}</h3>
@@ -26,7 +33,7 @@ function PostItem(props) {
         </div>
       </Link>
     </li>
-  )
+  );
 }
 
 export default PostItem;
