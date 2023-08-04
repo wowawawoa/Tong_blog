3a8a4b6ee53f513a324d7b223b226590764ed1cc
Tong Lin <wowawawoa@gmail.com>
2023-08-04 22:12:11 +0930

    feat: sending data from client to the api route and storing in mongodb

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,30 +1,71 @@
-import classes from './contact-form.module.css';
+import classes from "./contact-form.module.css";
+import { useState } from "react";
 
-function ContactForm () {
+function ContactForm() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    // optional: add client-side validation
+
+    fetch("api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
-            <label htmlFor='email'>Your Email</label>
-            <input type='email' id='email' required />
+            <label htmlFor="email">Your Email</label>
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(event) => setEnteredEmail(event.target.value)}
+            />
           </div>
           <div className={classes.control}>
-            <label htmlFor='name'>Your Name</label>
-            <input type='text' id='name' required />
+            <label htmlFor="name">Your Name</label>
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={(event) => setEnteredName(event.target.value)}
+            />
           </div>
         </div>
         <div className={classes.control}>
-          <label htmlFor='message'>Your Message</label>
-          <textarea id='message' rows='5'></textarea>
+          <label htmlFor="message">Your Message</label>
+          <textarea
+            id="message"
+            rows="5"
+            required
+            value={enteredMessage}
+            onChange={(event) => setEnteredMessage(event.target.value)}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
         </div>
       </form>
     </section>
-  )
+  );
 }
 
 export default ContactForm;
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (
@@ -13,18 +15,37 @@ function handler(req, res) {
       return;
     }
 
-    // Store it in a database
     const newMessage = {
       email,
       name,
       message,
     };
 
-    console.log(newMessage);
+    let client;
 
-    res
-      .status(201)
-      .json({ message: "Successfully stored message!", message: newMessage });
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://wowawawoa:Oaui8Jr6S8TbzOoC@cluster0.quye6t3.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
+
+    res.status(201).json({ message: newMessage });
   }
 }
 
