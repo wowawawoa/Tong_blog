807dd7b66d127a331bf2a8cfea3d9db1dba9e035
Tong Lin <wowawawoa@gmail.com>
2023-08-04 17:04:16 +0930

    feat: add post detail page

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,23 @@
+import classes from "./post-content.module.css";
+import PostHeader from "./post-header";
+
+const DUMMY_POST = {
+  slug: "getting-started-with-nextjs",
+  title: "Getting Started with NextJS",
+  image: "getting-started-nextjs.png",
+  date: "2023-08-04",
+  content: "# This is a first post",
+};
+
+function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  );
+}
+
+export default PostContent;
